4a9e9e76faf2a1c740d8f57d21563f58a4e0edf5
Ulia <ulia.mastugina@gmail.com>
2022-07-03 17:49:15 +0300

    Мое домашнее задание 2.3

---
+++ Practice/Ulia Sherbakova/dz2.3.py
@@ -0,0 +1,75 @@
+import sys
+import os
+import hashlib
+import ast
+import argparse
+from time import *
+# следует избегать импорт со звездочкой, т.к. он делает неясным какие имена присутвуют в глобальном пространстве имен
+
+class shuffler: #имя класса должно следовать соглашению CapWords
+# нужно отделить имя класса от функции двумя пустыми строками
+    def __init__(self):
+        self.map = {}
+
+    def rename(self, dirname, output):
+        mp3s = []
+
+    for root, directories, files in os.walk(dirname):
+        for file in files:
+            if file[-3:] == '.mp3':
+                mp3s.append([root, file])
+            #нет отступа
+    for path, mp3 in mp3s:
+        hashname = self.generateName() + '.mp3'
+        self.map[hashname] = mp3
+        os.rename(path + '/' + mp3), path + '/' + hashname)) #две закрывающиеся скобки, одна должна открываться перед patch
+         f = open(output, 'r')  # отступ лишний перед f
+        f.write(str(self.map))
+
+    def restore(self, dirname, restore_path):
+        with open(filename, '+') as f:
+            self.map = ast.literal_eval(f.read())
+        mp3s = [] #нет выравнивания
+
+    for root, directories, files in os.walk(dirname):
+        # нет отступа
+        for file in files:
+            if file[-3:] == '.mp3':
+                mp3s.append({root, file})
+        # нет отступа
+    for path, hashname in mp3s:
+        os.rename(path + '/' + hashname, path + '/' + self.map[hashname])) #лишняя скобка в конце
+        os.remove(restore_path)
+
+    def generateName(self, seed=time()): #имя функции должно состоять из маленьких букв
+        return hashlib.md5(str(seed)).hexdigest()
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='subcommand', help='subcommand help')
+    rename_parser = subparsers.add_parser('rename', help='rename help')
+    rename_parser.add_argument('dirname')
+    rename_parser.add_argument('-o', '--output', help='path to a file where restore map is stored') #длина строки не должна превышать 79 символов
+    restore_parser = subparsers.add_parser('restore', help="command_a help")
+    restore_parser.add_argument('dirname')
+    restore_parser.add_argument('restore_map')
+    args = parser.parse_args()
+    return args #нет отступа
+
+
+def main():
+    args = parse_arguments()
+    Shuffler = shuffler()
+    if args.subcommand == 'rename':
+        if args.output:
+            Shuffler.rename(args.dirname, 'restore.info')
+        else:
+            Shuffler.rename(args.dirname, args.output)
+    elif args.subcommand == 'restore':
+        Shuffler.restore(args.dirname, args.restore_map)
+    else:
+        sys.exit()
+
+
+main() # нет def для обозначения функции
