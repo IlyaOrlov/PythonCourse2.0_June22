271fd814b43bfa443a91cac3f591ece8acc98004
IlyaB <bebenin.ilya1994@gmail.com>
2022-07-04 17:51:28 +0800

    My work1

---
+++ Practice/BebeninIlya/Tema 2 Urok 1.py
@@ -0,0 +1,18 @@
+# import math
+# radius = input("Введите радиус: ")
+# def square(r):
+#     return math.pi * r ** 2
+# result = square(int(radius))
+# print(f"Площадь круга: {result}")
+
+result = square(int(radius))
+
+print(f"Площадь круга: {result}")
+
+def square(r):
+
+    return math.pi * r ** 2
+
+radius = input("Введите радиус: ")
+
+import math
