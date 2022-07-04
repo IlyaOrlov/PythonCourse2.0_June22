1470f669530c91580a9a72c9ead3db86444f71f0
IlyaB <bebenin.ilya1994@gmail.com>
2022-07-04 17:57:55 +0800

    My work1

---
+++ Practice/BebeninIlya/Tema Urok 2.py
@@ -0,0 +1,14 @@
+start = input("Топлива было: "); end = input("Топлива осталось: ")
+
+# start = input("Топлива было: ")
+# end = input("Топлива осталось: ")
+
+distance = input("Расстояние: ")
+diff = int(start) - int(end)
+result = diff / distance
+
+# result =  distance / diff
+
+print
+
+# print(result)
