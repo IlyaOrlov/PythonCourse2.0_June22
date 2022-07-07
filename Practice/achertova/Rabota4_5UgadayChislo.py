8794def6e13b02a387e93afc6665b226b58dce2e
aachertova <a.a.chertova94@gmail.com>
2022-07-07 16:28:52 +0300

    Мое домашнее задание 4.5

--- Practice/achertova/Rabota4_5UgadayChislo.py
+++ Practice/achertova/Rabota4_5UgadayChislo.py
@@ -4,17 +4,20 @@ s1 = input("Введите начало диапазона чисел: ")
 while not s1.isdecimal():
     print("Введите числовое целое значение!")
     s1 = input("Введите начало диапазона чисел еще раз: ")
+    break
 s1 = int(s1)
 s2 = input("Введите конец диапазона чисел: ")
 while not s2.isdecimal() or int(s1) >= int(s2):
     print("Числовое значение должно быть целым и большим по значению!")
     s2 = input("Введите конец диапазона чисел еще раз: ")
+    break
 s2 = int(s2)
 number = random.randint(s1, s2)
 guess = input("Угадайте значение внутри диапазона: ")
-while not guess.isdecimal() or int(guess) <= s1 or int(guess) >= s2:
+while not guess.isdecimal() or int(guess) < s1 or int(guess) > s2:
     print("Будьте внимательны! Введите целое числовое значение внутри диапазона!")
     guess = input("Введите значение еще раз: ")
+    break
 #Хотелось бы связать этот блок и 26 строку, но как-то не родилось, поэтому посчитала, что пользователь уже научился
 guess = int(guess)
 while guess != number:
