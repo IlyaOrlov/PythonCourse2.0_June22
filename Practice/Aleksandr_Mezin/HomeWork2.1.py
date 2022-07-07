d1699ef9b7f27c502323437fdd26f217f2c3db63
Admin <tehnixm@mail.ru>
2022-07-07 16:02:04 +0400

    My Homework2.11

--- Practice/Aleksandr_Mezin/HomeWork2.1.py
+++ Practice/Aleksandr_Mezin/HomeWork2.1.py
@@ -1,13 +1,23 @@
 import math
 
-def square(r):
-result = square(int(radius)
 
+def square(r):
+    return math.pi * r ** 2
+
+
+radius = input("Введите радиус: ")
+result = square(int(radius))
 print(f"Площадь круга: {result}")
 
 
 
-    return math.pi * r ** 2
 
-radius = input("Введите радиус: ")
+
+
+
+
+
+
+
+
 
