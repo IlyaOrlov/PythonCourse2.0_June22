3dc806bfe27d2a29fbe72e25bc73ae11ddb67c59
Ulia <ulia.mastugina@gmail.com>
2022-07-09 18:42:38 +0300

    Мое домашнее задание 4.5

---
+++ Practice/Ulia Sherbakova/dz4.5.py
@@ -0,0 +1,17 @@
+import random
+
+
+beg = int(input("Введите нижнюю границу диапазона: "))
+end = int(input("Введите верхнюю границу диапазона: "))
+while end < beg or end == beg:
+    print(f"Верхняя граница должна быть больше нижней")
+    break
+random = random.randint(beg, end)
+while (s := input("Угадай число из этого диапазона: ")).isdecimal():
+    if int(s) > random:
+        print(f"Число немного меньше {s}")
+    elif int(s) < random:
+        print(f"Число немного больше {s}")
+    elif int(s) == random:
+        print(f"Поздравляю, вы угадали")
+        break
