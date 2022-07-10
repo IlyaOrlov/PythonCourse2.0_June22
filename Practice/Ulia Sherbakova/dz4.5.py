26092683fbf29b532f809043532427bd1ccab209
Ulia <ulia.mastugina@gmail.com>
2022-07-10 21:13:06 +0300

    Мое домашнее задание 4.5

--- Practice/Ulia Sherbakova/dz4.5.py
+++ Practice/Ulia Sherbakova/dz4.5.py
@@ -3,7 +3,7 @@ import random
 beg = int(input("Введите нижнюю границу диапазона: "))
 end = int(input("Введите верхнюю границу диапазона: "))
 while end < beg or end == beg:
-    print(f"Верхняя граница должна быть больше нижней")
+    end = int(input("Верхняя граница должна быть больше нижней: "))
     break
 rand = random.randint(beg, end)
 while (s := input("Угадай число из этого диапазона: ")).isdecimal():
