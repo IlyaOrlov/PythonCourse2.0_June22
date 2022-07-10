deb9a7556760a4593e3d127bf6b27a8c3e1696a7
Admin <tehnixm@mail.ru>
2022-07-10 11:08:26 +0400

    готово 4.2 теперь и буквы нельзя вводить

--- Practice/Aleksandr_Mezin/HW4/4.2.py
+++ Practice/Aleksandr_Mezin/HW4/4.2.py
@@ -1,6 +1,6 @@
 a = input("Введите пятизначное число:")
 
-while len(str(a)) != 5:
+while len(str(a)) != 5 or not a.isdecimal():
     print("Неверное число: ")
     a = input("Введите пятизначное число:")
 i = 1
