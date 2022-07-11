c2b82c33114a8e709501a1bde0fa514502df8440
Pyatugin Vyacheslav <pvv114-1@mail.ru>
2022-07-11 17:43:58 +0300

    Исправил

--- Practice/pyatugin/check_pvv.py
+++ Practice/pyatugin/check_pvv.py
@@ -1,5 +1,5 @@
 x = input("Введите первое слагаемое: ")
-y = input("Ввеcbynfrcbc&дите второе слагаемое: ")
+y = input("Введите второе слагаемое: ")
 result = int(x) + int(y)
 result = result / 2
 print(f"Среднее арифметическое: {result}")
