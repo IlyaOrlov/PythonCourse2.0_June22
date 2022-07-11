3d67f5e9c8025eeeaf77f52074af7bc1e874ba22
Admin <tehnixm@mail.ru>
2022-07-11 15:44:51 +0400

    3.3 готово

--- Practice/Aleksandr_Mezin/HW4/4.3.py
+++ Practice/Aleksandr_Mezin/HW4/4.3.py
@@ -1,11 +1,10 @@
-import random
-
+s1 = ""
 while True:
     s = input("Введите число: ")
     if s.lower() == 'stop':
+        print(s1)
         break
     elif not s.isdecimal():
         print("Нужно ввести число")
     else:
-        s1 = random.choice(s)
-        print(s1)
+        s1 += s
