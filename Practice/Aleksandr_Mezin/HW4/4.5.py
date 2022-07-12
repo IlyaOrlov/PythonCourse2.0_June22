3b7cfe5067cb427d79f718f0bf4f9084b1777915
Admin <tehnixm@mail.ru>
2022-07-12 15:51:38 +0400

    4.5 оптимизировал строки 15, 18

--- Practice/Aleksandr_Mezin/HW4/4.5.py
+++ Practice/Aleksandr_Mezin/HW4/4.5.py
@@ -5,16 +5,14 @@ b = int(input("Введите второе число: "))
 i = range(a, b)
 c = (random.choice(i))
 a1 = input("Угадатайте число: ")
-
 while a1.isdecimal():
     if int(a1) == c:
         print("Поздравляю Вы угадали!! ")
         break
     elif int(a1) <= c:
         print("нет, число чуть больше")
-        a1 = input("Угадатайте снова: ")
     elif int(a1) >= c:
         print("нет, число чуть меньше")
-        a1 = input("Угадатайте снова: ")
     else:
         break
+    a1 = input("Угадатайте снова: ")
