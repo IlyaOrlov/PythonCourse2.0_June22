d539a5d0974a40230b6d5ea93cc55f5cc024f773
aachertova <a.a.chertova94@gmail.com>
2022-07-13 16:42:32 +0300

    Исправления в работе 4_5

--- Practice/achertova/Rabota4_5UgadayChislo.py
+++ Practice/achertova/Rabota4_5UgadayChislo.py
@@ -1,31 +1,34 @@
 import random
 
+
+def check_guess(g):
+    while not g.isdecimal() or int(g) < s1 or int(g) > s2:
+        print("Будьте внимательны! Введите целое числовое значение внутри диапазона!")
+        g = input("Введите значение еще раз: ")
+    return g
+
+
 s1 = input("Введите начало диапазона чисел: ")
 while not s1.isdecimal():
     print("Введите числовое целое значение!")
     s1 = input("Введите начало диапазона чисел еще раз: ")
-    break
 s1 = int(s1)
 s2 = input("Введите конец диапазона чисел: ")
 while not s2.isdecimal() or int(s1) >= int(s2):
     print("Числовое значение должно быть целым и большим по значению!")
     s2 = input("Введите конец диапазона чисел еще раз: ")
-    break
 s2 = int(s2)
 number = random.randint(s1, s2)
 guess = input("Угадайте значение внутри диапазона: ")
-while not guess.isdecimal() or int(guess) < s1 or int(guess) > s2:
-    print("Будьте внимательны! Введите целое числовое значение внутри диапазона!")
-    guess = input("Введите значение еще раз: ")
-    break
-#Хотелось бы связать этот блок и 26 строку, но как-то не родилось, поэтому посчитала, что пользователь уже научился
+guess = check_guess(guess)
 guess = int(guess)
 while guess != number:
     if guess < number:
         print("Загаданное число немного больше!")
     elif guess > number:
         print("Загаданное число немного меньше!")
-    guess = int(input("Попробуй еще: "))
+    guess = input("Попробуй еще: ")
+    guess = int(check_guess(guess))
 else:
     print("Победа! Вы угадали!")
 
