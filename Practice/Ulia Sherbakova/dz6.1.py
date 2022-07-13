b594820569a67b15910ccd9d74c732004f0cfedd
uliasherb <ulia.mastugina@gmail.com>
2022-07-13 22:29:59 +0300

    Мое домашнее задание 6.1

---
+++ Practice/Ulia Sherbakova/dz6.1.py
@@ -0,0 +1,13 @@
+class GreenTank:
+    model = "Т34"
+
+
+class BlackTank:
+    model = "Армата"
+
+
+e1 = GreenTank()
+e2 = BlackTank()
+
+print(f"Модель 1 танка: {e1.model}")
+print(f"Модель 2 танка: {e2.model}")
