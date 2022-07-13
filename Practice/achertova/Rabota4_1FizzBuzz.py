094725f88d39bea05ccaace0c4a07dcdb62ea162
aachertova <a.a.chertova94@gmail.com>
2022-07-13 16:51:19 +0300

    Исправления в работе 4_1

--- Practice/achertova/Rabota4_1FizzBuzz.py
+++ Practice/achertova/Rabota4_1FizzBuzz.py
@@ -1,11 +1,11 @@
 a = 1
 while a <= 100:
-    if a % 3 == 0 and a % 5 != 0:
-        print(f"Fizz")
-    elif a % 3 != 0 and a % 5 == 0:
-        print(f"Buzz")
-    elif a % 3 == 0 and a % 5 == 0:
+    if a % 15 == 0:
         print(f"FizzBuzz")
+    elif a % 3 == 0:
+        print(f"Fizz")
+    elif a % 5 == 0:
+        print(f"Buzz")
     else:
         print(a)
     a += 1
