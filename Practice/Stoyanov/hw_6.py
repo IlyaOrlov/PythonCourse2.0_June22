91ab6e1ff7ffa702392ef9d96daa8521e53adbed
Stoyanov Pavel <stoyanovrf@yandex.ru>
2022-07-14 15:16:31 +0300

    renamed files + hw_6

---
+++ Practice/Stoyanov/hw_6.py
@@ -0,0 +1,68 @@
+'''1. Спроектировать классы (один или несколько) для игры в танки и создать экземпляры этих классов.'''
+
+class Tank:
+    color = "Green"
+
+    def __init__(self, power, speed):
+        self.power = power
+        self.speed = speed
+
+    def move(self):
+        print("Танка поехал.")
+
+    def shoot(self):
+        print("Танка выстрелил.")
+
+
+t1 = Tank(500, 70)
+t2 = Tank(300, 100)
+t1.move()
+t2.shoot()
+print(Tank.color)
+
+'''2. Спроектировать класс Duck, при создании экземпляров которого будут задаваться атрибуты name, weight, 
+а атрибут color должен быть общим для всех экземпляров класса. Также в классе должны быть методы:
+- статический метод, выводящий «Сrack»;
+- классовый метод, выводящий цвет уток;
+- методы экземпляров: метод, выводящий имя и вес утки; метод __repl__ ; метод, принимающий 2х уток, и возвращающий 
+имя более тяжелой утки (__lt__); метод, сравнивающий вес 2х уток и возвращающий bool (равен вес или нет (__ne__)); 
+метод, суммирующий вес 2х уток(__add__).
+'''
+
+class Duck:
+    color = "White"
+
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def crack(self):
+        print("Crack!")
+
+    def colors(cls):
+        print(cls.color)
+
+    def __repr__(self):
+        return f'Эту утку зовут {self.name}, ее цвет {self.color}, ее вес {self.weight}'
+
+    def __ne__(self, other):
+        return self.weight == other.weight
+
+    def __lt__(self, other):
+        _a = self.weight < other.weight
+        return self.name
+
+    def __add__(self, other):
+        return self.weight + other.weight
+
+
+d1 = Duck("Donald", 50)
+d1.colors()
+d2 = Duck("Daisy", 30)
+d2.crack()
+ducks = (d1, d2)
+for i in ducks:
+    print(f'{i.__repr__()}')
+print(f"Утки одинаково весят?\n- {d1.__ne__(d2)}")
+print(f"Какая утка весит больше?\n- {d1.__lt__(d2)}")
+print(f"А сколько весят обе утки?\n- {d1.__add__(d2)}")
