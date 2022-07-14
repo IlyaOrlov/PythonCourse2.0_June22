096e8785288fe4366d33dce3051af2ef5c0700ef
uliasherb <ulia.mastugina@gmail.com>
2022-07-14 18:29:29 +0300

    Мое домашнее задание 6.1

--- Practice/Ulia Sherbakova/dz6.1.py
+++ Practice/Ulia Sherbakova/dz6.1.py
@@ -1,13 +1,16 @@
 class GreenTank:
     model = "Т34"
 
+    def __init__(self, speed, power):
+        self.speed = speed
+        self.power = power
 
-class BlackTank:
+class BlackTank(GreenTank):
     model = "Армата"
 
 
-e1 = GreenTank()
-e2 = BlackTank()
+e1 = GreenTank(50, 20)
+e2 = BlackTank(70, 10)
 
-print(f"Модель 1 танка: {e1.model}")
-print(f"Модель 2 танка: {e2.model}")
+print(f"Модель 1 танка: {e1.model}, его мощность {e1.power}, его скорость {e1.speed}")
+print(f"Модель 2 танка: {e2.model}, его мощность {e2.power}, его скорость {e2.speed}")
