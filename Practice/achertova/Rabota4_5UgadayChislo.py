3782dd2dec9dfb344b14c1191cf140e298ccc568
aachertova <a.a.chertova94@gmail.com>
2022-07-14 11:01:20 +0300

    Вторые исправления в работе 4_5

--- Practice/achertova/Rabota4_5UgadayChislo.py
+++ Practice/achertova/Rabota4_5UgadayChislo.py
@@ -19,16 +19,11 @@ while not s2.isdecimal() or int(s1) >= int(s2):
     s2 = input("Введите конец диапазона чисел еще раз: ")
 s2 = int(s2)
 number = random.randint(s1, s2)
-guess = input("Угадайте значение внутри диапазона: ")
-guess = check_guess(guess)
-guess = int(guess)
-while guess != number:
+while (guess := int(check_guess(input("Угадайте значение внутри диапазона: ")))) != number:
     if guess < number:
         print("Загаданное число немного больше!")
     elif guess > number:
         print("Загаданное число немного меньше!")
-    guess = input("Попробуй еще: ")
-    guess = int(check_guess(guess))
 else:
     print("Победа! Вы угадали!")
 
