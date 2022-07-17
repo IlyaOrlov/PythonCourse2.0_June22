557a16480828e9c30fcf3e31590c540829501f89
aachertova <a.a.chertova94@gmail.com>
2022-07-17 20:59:08 +0300

    Домашняя работа 6_2

---
+++ Practice/achertova/Rabota6_2Duck.py
@@ -0,0 +1,52 @@
+class Duck:
+    color = "grey"
+
+    def __init__(self, n, w):
+        self.name = n
+        self.weight = w
+
+    @staticmethod
+    def voice():
+        print(f"Сrack")
+
+    @classmethod
+    def duckcolor(cls):
+        print(cls.color)
+
+    def whoyouare(self):
+        print(f"I`m {self.name}. My weight is {self.weight}")
+
+    def __repr__(self):
+        return f"{self.color} Duck {self.name}"
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+
+    def who_is_heavier(self, other, a):
+        if a := True:
+            print(f"{other.name}")
+        else:
+            print(f"{self.name}")
+
+    def __ne__(self, other):
+        return self.weight != other.weight
+
+    def bool(self, b):
+        return bool(b)
+    # ne и bool не совсем поняла, что требуется в задании. Сделала так, что через ne проверяется неравенство,
+    # а через bool выводится True(Не равны) или False(Равны)
+
+    def __add__(self, other):
+        return self.weight + other.weight
+
+d1 = Duck("Gena", 3)
+d2 = Duck("Terminator", 7)
+Duck.duckcolor()
+a = d1 < d2
+Duck.who_is_heavier(d1, d2, a)
+b = Duck.__ne__(d1, d2)
+print(bool(b))
+print(d1 + d2)
+
+
