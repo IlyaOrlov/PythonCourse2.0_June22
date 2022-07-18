580a077038521beb85222adad12d0b5404d653f2
Stoyanov Pavel <stoyanovrf@yandex.ru>
2022-07-18 11:40:50 +0300

    fixed

--- Practice/Stoyanov/hw_6.py
+++ Practice/Stoyanov/hw_6.py
@@ -1,24 +1,26 @@
 '''1. Спроектировать классы (один или несколько) для игры в танки и создать экземпляры этих классов.'''
 
-class Tank:
-    color = "Green"
-
-    def __init__(self, power, speed):
-        self.power = power
-        self.speed = speed
-
-    def move(self):
-        print("Танка поехал.")
-
-    def shoot(self):
-        print("Танка выстрелил.")
-
-
-t1 = Tank(500, 70)
-t2 = Tank(300, 100)
-t1.move()
-t2.shoot()
-print(Tank.color)
+# class Tank:
+#     color = "Green"
+#
+#     def __init__(self, power, speed):
+#         self.power = power
+#         self.speed = speed
+#
+#     @staticmethod
+#     def move():
+#         print("Танка поехал.")
+#
+#     @staticmethod
+#     def shoot():
+#         print("Танка выстрелил.")
+#
+#
+# t1 = Tank(500, 70)
+# t2 = Tank(300, 100)
+# t1.move()
+# t2.shoot()
+# print(Tank.color)
 
 '''2. Спроектировать класс Duck, при создании экземпляров которого будут задаваться атрибуты name, weight, 
 а атрибут color должен быть общим для всех экземпляров класса. Также в классе должны быть методы:
@@ -36,9 +38,11 @@ class Duck:
         self.name = name
         self.weight = weight
 
-    def crack(self):
+    @staticmethod
+    def crack():
         print("Crack!")
 
+    @classmethod
     def colors(cls):
         print(cls.color)
 
@@ -46,11 +50,15 @@ class Duck:
         return f'Эту утку зовут {self.name}, ее цвет {self.color}, ее вес {self.weight}'
 
     def __ne__(self, other):
-        return self.weight == other.weight
+        return self.weight != other.weight
 
     def __lt__(self, other):
-        _a = self.weight < other.weight
-        return self.name
+        # _a = self.weight < other.weight
+        # return self.name
+        if self.weight < other.weight:
+            return other.name
+        else:
+            return self.name
 
     def __add__(self, other):
         return self.weight + other.weight
@@ -62,7 +70,7 @@ d2 = Duck("Daisy", 30)
 d2.crack()
 ducks = (d1, d2)
 for i in ducks:
-    print(f'{i.__repr__()}')
-print(f"Утки одинаково весят?\n- {d1 == d2}")
+    print(i)
+print(f"Утки разного веса?\n- {d1 != d2}")
 print(f"Какая утка весит больше?\n- {d1 < d2}")
 print(f"А сколько весят обе утки?\n- {d1 + d2}")
