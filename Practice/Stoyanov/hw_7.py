914418515876f6eebc5937c21e16d478d9a18ab2
Stoyanov Pavel <stoyanovrf@yandex.ru>
2022-07-18 12:54:00 +0300

    Stoyanov

---
+++ Practice/Stoyanov/hw_7.py
@@ -0,0 +1,41 @@
+'''1. Написать класс Man, который принимает имя в конструкторе. Имеет метод solve_task, который просто выводит
+"I'm not ready yet".
+'''
+
+class Man:
+
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def solve_task():
+        print("I'm not ready yet.")
+
+
+name = input("Введите ваше имя: ")
+m = Man(name)
+print(m.name)
+m.solve_task()
+
+'''2. Написать класс Pupil, у которого переопределен метод solve_task. На этот раз он будет думать от 3 до 6 секунд 
+(c помощью метода sleep библиотеки time и randint библиотеки random).
+'''
+from time import sleep
+from random import randint
+
+
+class Pupil:
+
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def solve_task():
+        print(f"{name} думает...")
+        sleep(randint(3, 6))
+        print("I'm not ready yet.")
+
+
+name = input("Введите ваше имя: ")
+p = Pupil(name)
+p.solve_task()
