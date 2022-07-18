69d9d8e4d9c78cddefbc7f7f15a040c5f1a74b65
uliasherb <ulia.mastugina@gmail.com>
2022-07-18 17:49:40 +0300

    Мое домашнее задание 7.1

---
+++ Practice/Ulia Sherbakova/dz7.1.py
@@ -0,0 +1,10 @@
+class Man:
+    def __init__(self, name):
+        self.name = name
+
+    def solve_task(self):
+        print(f"I'm not ready yet.")
+
+man1 = Man("ivan")
+man1.solve_task()
+print(man1.name)
