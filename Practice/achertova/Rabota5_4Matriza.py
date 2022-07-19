54bf522d28e3fde01b6ffc9a03e6e30c998d2523
aachertova <a.a.chertova94@gmail.com>
2022-07-19 15:52:03 +0300

    Домашняя работа 6_1 Исправлено.2

--- Practice/achertova/Rabota5_4Matriza.py
+++ Practice/achertova/Rabota5_4Matriza.py
@@ -1,15 +1,14 @@
 m = [[1, 2, 3],
      [5, 5, 6],
      [7, 8, 9]]
-m2 = m
 a = 5
 index1 = 0
 index2 = 0
 while index2 < len(m[0]):
-    stolbik = [row[index2] for row in m2]
+    stolbik = [row[index2] for row in m]
     if a in stolbik:
         index1 = 0
-        while index1 < 3:
+        while index1 < len(m):
             del m[index1][index2]
             index1 += 1
     else:
