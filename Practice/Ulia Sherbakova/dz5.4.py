21954ad2dcce96eab1ad9349213bc63a6158ec51
uliasherb <ulia.mastugina@gmail.com>
2022-07-19 17:28:14 +0300

    Мое домашнее задание 5.4

--- Practice/Ulia Sherbakova/dz5.4.py
+++ Practice/Ulia Sherbakova/dz5.4.py
@@ -1,12 +1,17 @@
+import numpy as np
+
 def fun(arr):
-    a = 5
+    a = 9
     s = len(arr)
     c = 0
+    new = []
     while c < s - 1:
         for i in arr:
             for j in i:
                 if j == a:
-                    arr.pop(c)
+                    new.append(i)
+                    print(new)
+                    np.delete(arr, new, 0)
         c += 1
     print(arr)
 
