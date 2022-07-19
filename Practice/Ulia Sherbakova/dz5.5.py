8f1ce883d7762ab8fba9ac1f9ab0f5c4a9a35e40
uliasherb <ulia.mastugina@gmail.com>
2022-07-19 10:51:09 +0300

    Мое домашнее задание 5.5

--- Practice/Ulia Sherbakova/dz5.5.py
+++ Practice/Ulia Sherbakova/dz5.5.py
@@ -1,12 +1,12 @@
 a = input("Введите 't' для сворачивания и 'k' для разворачивания табуляции: ")
 
 with open("1.txt", "r") as f1, open("2.txt", "w") as f2:
-        for i in f1:
-            if a == 't':
-                s = repr(i.replace("\t", "    "))
-                f2.write(s)
-            elif a == 'k':
-                s = repr(i.replace("    ", "\t"))
-                f2.write(s)
-            else:
-                print("Ошибка! Введите 't' или 'k'")
+    for i in f1:
+        if a == 't':
+            s = repr(i.replace("\t", "    "))
+            f2.write(s)
+        elif a == 'k':
+            s = repr(i.replace("    ", "\t"))
+            f2.write(s)
+        else:
+            print("Ошибка! Введите 't' или 'k'")
