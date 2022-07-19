956eb5c94dd0e430b50781cb516d15bfb8a2127c
aachertova <a.a.chertova94@gmail.com>
2022-07-19 10:30:42 +0300

    Домашняя работа 5_3 Исправлено

--- Practice/achertova/Rabota5_3ZamenaShablona.py
+++ Practice/achertova/Rabota5_3ZamenaShablona.py
@@ -1,6 +1,5 @@
 s = "Вчера я посетил Макдональдс. Пил там Колу и ел МакЧикен"
 impotozameschenie = {"Макдональдс.": "Вкусно и точка.", "Колу": "Черноголовку", "МакЧикен": "Наггетсы"}
-s2 = s.split(" ")
 for k, v in impotozameschenie.items():
     s = s.replace(k, v)
 print(s)
