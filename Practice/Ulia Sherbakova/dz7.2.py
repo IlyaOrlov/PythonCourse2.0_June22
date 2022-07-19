babb077bdec28c1a85cbd35747d7e401823dbcd3
uliasherb <ulia.mastugina@gmail.com>
2022-07-19 17:58:37 +0300

    Мое домашнее задание 7.2

--- Practice/Ulia Sherbakova/dz7.2.py
+++ Practice/Ulia Sherbakova/dz7.2.py
@@ -1,20 +1,28 @@
 import time
 import random
 
+
 class Man:
+
     def __init__(self, name):
         self.name = name
 
-    def solve_task(self):
+    @staticmethod
+    def solve_task():
         print(f"I'm not ready yet.")
 
+
 class Pupil(Man):
-    s = random.randint(3, 6)
-    time.sleep(s)
+
+    def solve_task_n(self):
+        s = random.randint(3, 6)
+        time.sleep(s)
+        super().solve_task()
+
 
 man1 = Man("ivan")
 man1.solve_task()
 print(man1.name)
 man2 = Pupil("Egor")
-man2.solve_task()
+man2.solve_task_n()
 print(man2.name)
