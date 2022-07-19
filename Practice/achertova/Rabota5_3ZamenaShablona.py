51e03abb167fbf2f12f86ba25d76e319c053abd0
aachertova <a.a.chertova94@gmail.com>
2022-07-19 10:28:57 +0300

    Домашняя работа 5_3-4 Исправлено

--- Practice/achertova/Rabota5_3ZamenaShablona.py
+++ Practice/achertova/Rabota5_3ZamenaShablona.py
@@ -1,10 +1,7 @@
 s = "Вчера я посетил Макдональдс. Пил там Колу и ел МакЧикен"
 impotozameschenie = {"Макдональдс.": "Вкусно и точка.", "Колу": "Черноголовку", "МакЧикен": "Наггетсы"}
 s2 = s.split(" ")
-for i in s2:
-    for k, v in impotozameschenie.items():
-        if i == k:
-            s = s.replace(i, v)
+for k, v in impotozameschenie.items():
+    s = s.replace(k, v)
 print(s)
 
-#Готово
--- Practice/achertova/Rabota5_4Matriza.py
+++ Practice/achertova/Rabota5_4Matriza.py
@@ -1,16 +1,17 @@
 m = [[1, 2, 3],
-     [4, 5, 6],
+     [5, 5, 6],
      [7, 8, 9]]
-a = 9
+m2 = m
+a = 5
 index1 = 0
 index2 = 0
-while index2 < 3:
-      stolbik = [row[index2] for row in m]
-      if a in stolbik:
-            while index1 < 3:
-                  del m[index1][index2]
-                  index1 += 1
-            break
-      index2 += 1
+while index2 < len(m[0]):
+    stolbik = [row[index2] for row in m2]
+    if a in stolbik:
+        index1 = 0
+        while index1 < 3:
+            del m[index1][index2]
+            index1 += 1
+    else:
+        index2 += 1
 print(m)
-# Готово
