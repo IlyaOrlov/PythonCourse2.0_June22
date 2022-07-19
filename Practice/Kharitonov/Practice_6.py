2f9b4d4fa302c48ec8f26b7181cea9984a440308
Onix125 <onix125@ya.ru>
2022-07-19 23:28:05 +0300

    Alexey Kharitonov

--- Practice/Kharitonov/Practice_6.py
+++ Practice/Kharitonov/Practice_6.py
@@ -3,30 +3,27 @@ class Tank():
     def __init__(self, name, speed, damag):
         self.name = name
         self.speed = speed
+
         self.damag = damag
 
-    def show_Tank(self):
+    def show_tank(self):
         print(f'Имя {self.name} Скорость: {self.speed} Урон: {self.damag} ')
 
     def move(self):
         print("Tank " + self.name + " -> Движение")
 
-    def lev_up_1(self):
+    def lev_up(self):
         self.speed += 20
-        print(f'Скорость Танка ИСУ-152 +20 = {self.speed}')
-
-    def lev_up_2(self):
-        self.speed += 20
-        print(f'Скорость Танка CY-100 +20 = {self.speed}')
+        print(f'Скорость Танка {self.name} +20 = {self.speed}')
 
 
 tank_1 = Tank('ИСУ-152', 40, 100)
 tank_2 = Tank('CY-100', 60, 70)
 
 
-tank_1.show_Tank()
-tank_2.show_Tank()
+tank_1.show_tank()
+tank_2.show_tank()
 tank_1.move()
 tank_2.move()
-tank_2.lev_up_2()
-tank_1.lev_up_1()
+tank_2.lev_up()
+tank_1.lev_up()
