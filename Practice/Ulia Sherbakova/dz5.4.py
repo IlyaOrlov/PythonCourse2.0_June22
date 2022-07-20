3be9d32840378810cce5685d5123adc77978646f
uliasherb <ulia.mastugina@gmail.com>
2022-07-20 12:12:22 +0300

    Мое домашнее задание 5.4

--- Practice/Ulia Sherbakova/dz5.4.py
+++ Practice/Ulia Sherbakova/dz5.4.py
@@ -1,17 +1,20 @@
-def fun(arr):
-    a = 20
+def fun(arr, a):
     s = len(arr)
-    count1 = 0
     count2 = 0
-    while count1 < s - 1 and count2 < s - 1:
-        for i in arr:
-            for j in i:
-                if j == a:
-                    del(arr[count1][count2])
-                    count2 += 1
-            else:
+    new = []
+    while count2 < len(arr[count2]):
+        new.clear()
+        for row in arr:
+            new.append(row[count2])
+        if a in new:
+            count1 = 0
+            while count1 < s:
+                del(arr[count1][count2])
                 count1 += 1
+        else:
+            count2 += 1
     print(arr)
 
-arr = [[1, 20, 20], [18, 21, 18],[21, 20, 19]]
-fun(arr)
+arr = [[20, 20, 20], [18, 21, 18],[21, 20, 19]]
+a = 20
+fun(arr, a)
