b4c865fbe7223dbad33c7316776c7c5f60a5fbd5
Stoyanov Pavel <stoyanovrf@yandex.ru>
2022-07-22 11:13:58 +0300

    Fixed

--- Practice/Stoyanov/hw_7.py
+++ Practice/Stoyanov/hw_7.py
@@ -47,7 +47,7 @@
 количестве наличных и наборе поддерживаемых операций.
 """
 
-class Bancomat_old:
+class BancomatOld:
 
     def __init__(self, cash):
         self.cash = cash
@@ -58,56 +58,53 @@ class Bancomat_old:
         print("-" * 10)
 
     def put_money(self, money):
-        if money > 0:
-            self.cash += money
-            print(f"Вы внесли {money}р")
-        else:
-            print("Ошибка! Сумма должна быть больше 0!")
+        self.cash += abs(money)
+        print(f"Вы внесли {abs(money)}р")
 
     def give_money(self, money):
-        if self.cash - money > 0:
-            self.cash -= money
-            print(f"Вы сняли {money}р")
+        if self.cash - abs(money) > 0:
+            self.cash -= abs(money)
+            print(f"Вы сняли {abs(money)}р")
         else:
             print(f"Недостаточно средств! В банкомате всего {self.cash}")
 
     def online(self, money):
         print("Данный банкомат не умеет совершать онлайн платежи!")
 
-    def info(self):
-        print(f"Баланс банкомата - {self.cash}\nБанкомат умеет только принимать и выдавать деньги.")
-        print("-" * 10)
+    def __repr__(self):
+        return f"{'-'*10}\nБаланс банкомата - {self.cash}\nБанкомат умеет принимать, выдавать деньги."
 
-class Bancomat_new(Bancomat_old):
+
+class BancomatNew(BancomatOld):
 
     def online(self, money):
-        if self.cash - money > 0:
-            self.cash -= money
-            print(f"Вы совершили онлайн платеж на {money}р")
+        if self.cash - abs(money) > 0:
+            self.cash -= abs(money)
+            print(f"Вы совершили онлайн платеж на {abs(money)}р")
         else:
             print("Недостаточно средств!")
 
-    def info(self):
-        print(f"Баланс банкомата - {self.cash}\nБанкомат умеет принимать, выдавать деньги и совершать онлайн платежи.")
-        print("-"*10)
+    def __repr__(self):
+        print(super().__repr__())
+        return "Еще банкомат умеет совершать онлайн платежи."
 
-b1 = Bancomat_old(1000)
-b1.put_money(589)
+
+b1 = BancomatOld(1000)
+b1.put_money(-589)
 b1.balance()
-b2 = Bancomat_old(2000)
-b2.give_money(1340)
+b2 = BancomatOld(2000)
+b2.give_money(-1340)
 b2.balance()
-b3 = Bancomat_old(15000)
+b3 = BancomatOld(15000)
 b3.online(200)
 b3.balance()
-nb1 = Bancomat_new(9000)
+nb1 = BancomatNew(9000)
 nb1.online(50)
 nb1.balance()
-nb2 = Bancomat_new(1000)
+nb2 = BancomatNew(1000)
 nb2.give_money(700)
 nb2.balance()
 
 lst = [b1, nb2, b2, b3, nb1]
 for i in lst:
-    i.info()
-
+    print(i)
