69557f4d6d3c581200cd1089c74a1ec955c43d86
uliasherb <ulia.mastugina@gmail.com>
2022-07-22 19:10:11 +0300

    Мое домашнее задание 8.3

---
+++ Practice/Ulia Sherbakova/dz8.3.py
@@ -0,0 +1,20 @@
+import time
+
+
+class TestManager:
+    def __enter__(self):
+        print('Starting code inside manager')
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Code completed.')
+
+
+with TestManager():
+    startTime = time.time()
+    s = 'строкадлязаменысимволов'
+    x = {'а': 3, 'з': 5, 'о': 7}
+    for key in x.keys():
+        s = s.replace(key, str(x[key]))
+    print(s)
+    endTime = time.time()
+    totalTime = endTime - startTime
+    print("Время, затраченное на выполнение данного кода = ", totalTime)
