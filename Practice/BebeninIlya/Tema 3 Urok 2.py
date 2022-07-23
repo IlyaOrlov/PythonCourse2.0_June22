e7214f97db47e331935fa1465d7bce5cf0cbd83e
IlyaB <bebenin.ilya1994@gmail.com>
2022-07-23 21:23:50 +0800

    My work1

--- Practice/BebeninIlya/Tema 3 Urok 2.py
+++ Practice/BebeninIlya/Tema 3 Urok 2.py
@@ -1,4 +1,4 @@
-a= input("Введите сторону a = ")
-b= input("Введите сторону b = ")
+a = input("Введите сторону a = ")
+b = input("Введите сторону b = ")
 Perimetr = (int(a)+ int(b))*2
 print(Perimetr)
