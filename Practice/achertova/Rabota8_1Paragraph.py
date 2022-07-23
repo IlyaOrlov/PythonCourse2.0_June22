7bd846f10f0df9561ffad2fe8b858a61bcaff97b
aachertova <a.a.chertova94@gmail.com>
2022-07-23 11:21:51 +0300

    Домашняя работа 8_1

---
+++ Practice/achertova/Rabota8_1Paragraph.py
@@ -0,0 +1,38 @@
+class MyIter:
+	def __init__(self, str):
+		self.j = 0
+		self.i = 0
+		self.str = str
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		if self.i < len(str):
+			while str[self.i] != "/":
+				self.i += 1
+			else:
+				a = str[self.j:self.i]
+				self.j = self.i
+				self.i += 1
+				return a
+		else:
+			raise StopIteration
+
+str = "Python /стал /одним из самых /популярных языков/"
+mi = MyIter(str)
+it = iter(mi)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
+
+# str = "Python /стал /одним из самых /популярных языков/"
+# j = 0
+# for i in range(len(str)):
+# 	if str[i] == "/":
+# 		print(str[j:i])
+# 		j = i
+
+
